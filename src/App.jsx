9fb5b382683ea24f5176ab0120895021ea4e6a8b
AMOS KIPKORIR <amos.kipkorir@student.moringaschool.com>
2025-07-23 14:38:25 +0300

    Add comprehensive event listings including fashion, models,  cultural, and popular events

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Events from './components/Events'
 
 
 function App() {
   return( 
-    <></>
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+      <Events />
+    </div>
   
   )
 }
